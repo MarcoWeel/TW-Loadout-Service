60d5acbf9e1273f1e06bb263d6da7c6e0fe20454
MarcoWeel <deathstarmarco@gmail.com>
2021-01-07 16:16:35 +0100

    changed ip and added feign

--- account-api/src/main/java/com/tacticalwolves/accountapi/AccountApiApplication.java
+++ account-api/src/main/java/com/tacticalwolves/accountapi/AccountApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients("com.tacticalwolves.accountapi.proxies")
 public class AccountApiApplication {
 
     public static void main(String[] args) {
